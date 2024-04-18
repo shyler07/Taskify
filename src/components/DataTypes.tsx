9514ff21d03c027a592baff60008067f35e8765b
shyler07 <113320798+shyler07@users.noreply.github.com>
2024-04-18 14:59:57 +0800

    build

--- src/components/DataTypes.tsx
+++ src/components/DataTypes.tsx
@@ -1,51 +1,51 @@
 
-const DataTypes = () => {
-    let named: string;
-    let age : number | string ;// | sign is union you can put number and string
-    let isStudent: boolean;
-    let hobbies: string[];
-    let numberofHobbies: number[];
-    let role: [number, string];
-    let personName : unknown // use unnkown instead of any 
+// const DataTypes = () => {
+//     let named: string;
+//     let age : number | string ;// | sign is union you can put number and string
+//     let isStudent: boolean;
+//     let hobbies: string[];
+//     let numberofHobbies: number[];
+//     let role: [number, string];
+//     let personName : unknown // use unnkown instead of any 
     
-    // object in type script
-    type Person = {
-      name: string;
-      age: number;
-    }
+//     // object in type script
+//     type Person = {
+//       name: string;
+//       age: number;
+//     }
     
-    // interface in typescript extend property in other object
-    interface Guy extends Person {
-     profession: string;
-    }
-    
-    
-    let person: Person = {
-      name: "tyler",
-      age: 22
-    }
-    
-    let lotsOfPerson: Person[]
+//     // interface in typescript extend property in other object
+//     interface Guy extends Person {
+//      profession: string;
+//     }
     
     
-    // typescript function
-    function printName(name: string){
-      console.log(name)
-    }
+//     let person: Person = {
+//       name: "tyler",
+//       age: 22
+//     }
     
-    printName("tyler")
+//     let lotsOfPerson: Person[]
     
     
-    let newName : (name:string) => void; // you can use void or never
+//     // typescript function
+//     function printName(name: string){
+//       console.log(name)
+//     }
+    
+//     printName("tyler")
+    
+    
+//     let newName : (name:string) => void; // you can use void or never
 
 
-  return (
-    <div>
-      <p> datatypes</p>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <p> datatypes</p>
+//     </div>
+//   )
+// }
 
-export default DataTypes
+// export default DataTypes
 
 
